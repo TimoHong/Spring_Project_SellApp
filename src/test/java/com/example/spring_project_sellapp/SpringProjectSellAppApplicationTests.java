69353b22426af7ad2ac526d241cf56cf97da36fd
TimoHong <h.chiuwing@gmail.com>
2022-06-07 03:07:40 +0800

    Initial commit

---
+++ src/test/java/com/example/spring_project_sellapp/SpringProjectSellAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_project_sellapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectSellAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
